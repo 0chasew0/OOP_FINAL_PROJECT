c45cede6503451b6f040acf49d2912d9a175f72f
Chase <0chasew0@gmail.com>
2020-04-15 11:15:04 -0700

    Add project files.

---
+++ Ship.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+class Ship
+{
+public:
+	std::string shipType;
+	std::string shipPosition;
+	std::string shipOrientation;
+	bool shipSunk;
+	
+};
+
---
+++ UserGrid.cpp
@@ -0,0 +1,1 @@
+#include "UserGrid.h"
